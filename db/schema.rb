521c8d6e0b7cc7a6ec9c3abdc5cec53a6dd7b0f6
Jake Marwil <rjm02006@gmail.com>
2015-04-18 11:09:36 -0600

    generate devise elements and migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150418165417) do
+ActiveRecord::Schema.define(version: 20150418170715) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
-    t.string   "email"
     t.string   "avatar"
     t.string   "location"
     t.string   "work"
@@ -27,6 +26,19 @@ ActiveRecord::Schema.define(version: 20150418165417) do
     t.string   "interest"
     t.text     "about"
     t.integer  "spark_count"
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.inet     "current_sign_in_ip"
+    t.inet     "last_sign_in_ip"
   end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
 end
