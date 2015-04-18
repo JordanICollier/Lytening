be8a085c28081ada7463b3276de632bf53b20434
Jordan Collier <jordanicollier@gmail.com>
2015-04-18 15:21:18 -0600

    det method to private and set before action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,12 +3,14 @@ class UsersController < ApplicationController
   before_action :set_user
   respond_to :html, :json
 
+
+
+  def show
+
+  end
+
+private
   def set_user
     @user = User.find(params[:id])
   end
-
-  def show
-    
-  end
-
 end
