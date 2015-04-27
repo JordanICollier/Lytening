67a3305e05b08ff07eacb071c8cf0a2eed3b3f90
Jake Marwil <rjm02006@gmail.com>
2015-04-27 10:03:20 -0600

    nest strykes routes under user

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :users, only: [:show, :edit, :update]
+  resources :users, only: [:show, :edit, :update] do
+    resources :strykes
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
