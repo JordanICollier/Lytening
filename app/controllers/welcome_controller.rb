e4a78ac14c260ba93aaa0ea78d39829156343b31
Jake Marwil <rjm02006@gmail.com>
2015-05-19 14:39:00 -0600

    add ordering on strykes

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,8 @@
 class WelcomeController < ApplicationController
 
   def index
+    @strykes_hot = Stryke.order(created_at: :asc).order(spark_count: :asc).distinct
+    @strykes_new = Stryke.order(created_at: :desc).distinct.all
   end
 
 end
