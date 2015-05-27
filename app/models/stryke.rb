9093f723ff4e9f350558019ecfe5ea967c66cc56
Jake Marwil <rjm02006@gmail.com>
2015-05-27 13:59:34 -0600

    comment out hotness algorithm

--- app/models/stryke.rb
+++ app/models/stryke.rb
@@ -3,19 +3,20 @@ class Stryke < ActiveRecord::Base
   belongs_to :user
   has_many :comments
 
-  def hotness(stryke)
-    s = stryke.created_at
-    s = s + 2*stryke.spark_count
-    baseScore = log(max(s,1))
-
-    timeDiff = (Date.now - stryke.created_at).toWeeks
-
-    if (timeDiff > 1)
-      x = timeDiff - 1
-      baseScore = baseScore * exp(-8*x*x)
-    end
-
-    baseScore
-  end
+  # Old hotness algorithm
+  # def hotness(stryke)
+  #   s = stryke.created_at
+  #   s = s + 2*stryke.spark_count
+  #   baseScore = log(max(s,1))
+  #
+  #   timeDiff = (Date.now - stryke.created_at).toWeeks
+  #
+  #   if (timeDiff > 1)
+  #     x = timeDiff - 1
+  #     baseScore = baseScore * exp(-8*x*x)
+  #   end
+  #
+  #   baseScore
+  # end
 
 end
