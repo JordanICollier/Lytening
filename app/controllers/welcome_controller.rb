dc1308abbd1d9fb813bd75fe03a173292582ee44
Jake Marwil <rjm02006@gmail.com>
2015-05-28 11:28:22 -0600

    grab strykes within 24 hours from current time

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,8 +1,21 @@
 class WelcomeController < ApplicationController
 
   def index
+    @strykes_hot_recent = []
     @strykes_hot = Stryke.order(created_at: :asc).order(spark_count: :asc).distinct
+    @strykes_hot.each do |stryke|
+      if Time.now.utc.to_i - stryke.created_at.to_i < 100000
+        @strykes_hot_recent << stryke
+      end
+    end
+
+    @strykes_new_recent = []
     @strykes_new = Stryke.order(created_at: :desc).distinct.all
+    @strykes_new.each do |stryke|
+      if Time.now.utc.to_i - stryke.created_at.to_i < 100000
+        @strykes_new_recent << stryke
+      end
+    end
   end
 
 end
