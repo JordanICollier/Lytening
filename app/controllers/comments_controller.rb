d4a54280626e2a613842a57da35df8afee465167
Jake Marwil <rjm02006@gmail.com>
2015-05-28 14:21:34 -0600

    change @user.id to be current_user.id

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -7,9 +7,8 @@ class CommentsController < ApplicationController
   def create
     @comment = Comment.new(comment_params)
     @stryke = Stryke.find(params[:stryke_id])
-    @user = User.find(params[:user_id])
     @comment.stryke_id = @stryke.id
-    @comment.user_id = @user.id
+    @comment.user_id = current_user.id
     @comment.spark_count = 0
     if @comment.save
       render :json => @comment
