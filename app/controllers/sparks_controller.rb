d000c71f3404417a9dd79b2d216b424bc88917b5
Jake Marwil <rjm02006@gmail.com>
2015-06-02 13:00:55 -0600

    refactor spark increments/decrements in controller

--- app/controllers/sparks_controller.rb
+++ app/controllers/sparks_controller.rb
@@ -1,15 +1,36 @@
 class SparksController < ApplicationController
 
+  def new
+    @spark = Spark.new
+  end
+
   def create
     @spark = Spark.new(spark_params)
-    @stryke = Stryke.find(params[:stryke_id])
-    @comment = Comment.find(params[:comment_id])
-    @user = User.find(params[:user_id])
-    @spark.self_id = current_user.id
-    @spark.stryke_id = @stryke.id
-    @spark.comment_id = @comment.id
-    @spark.user_id = @user.id
-    render nothing: true
+
+    @spark.user_id = current_user.id
+    @spark.stryke_id = spark_params[:stryke_id]
+    @spark.comment_id = spark_params[:comment_id]
+    @spark.save
+
+    if params[:stryke_id] == ""
+      @comment = Comment.find(spark_params[:comment_id])
+      @comment.spark_count += 1
+      @comment.save
+
+      @user = User.find(@comment.user_id)
+      @user.spark_count += 1
+      @user.save
+    else
+      @stryke = Stryke.find(spark_params[:stryke_id])
+      @stryke.spark_count += 1
+      @stryke.save
+
+      @user = User.find(@stryke.user_id)
+      @user.spark_count += 1
+      @user.save
+    end
+
+    render json: @spark.to_json
   end
 
   def show
@@ -18,8 +39,28 @@ class SparksController < ApplicationController
   end
 
   def destroy
-    @spark = current_user.sparks.find(params[:id])
+    @spark = Spark.find(params[:id])
+
+    if params[:stryke_id] == ""
+      @comment = Comment.find(@spark.comment_id)
+      @comment.spark_count -= 1
+      @comment.save
+
+      @user = User.find(@comment.user_id)
+      @user.spark_count -= 1
+      @user.save
+    else
+      @stryke = Stryke.find(@spark.stryke_id)
+      @stryke.spark_count -= 1
+      @stryke.save
+
+      @user = User.find(@stryke.user_id)
+      @user.spark_count -= 1
+      @user.save
+    end
+
     @spark.destroy
+
     render nothing: true
   end
 
