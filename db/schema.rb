0f9d4e94cd0eb40f89f5085ca60379f5b1f0f65f
Jake Marwil <rjm02006@gmail.com>
2015-06-02 14:50:47 -0600

    add username to users table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150602175501) do
+ActiveRecord::Schema.define(version: 20150602204302) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -71,6 +71,7 @@ ActiveRecord::Schema.define(version: 20150602175501) do
     t.inet     "last_sign_in_ip"
     t.string   "first_name"
     t.string   "last_name"
+    t.string   "username"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
