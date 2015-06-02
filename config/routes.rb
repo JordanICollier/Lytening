a2f188c902d5d7d58e981b8730f250d19f7a9215
Jake Marwil <rjm02006@gmail.com>
2015-06-02 01:11:33 -0600

    add resources for sparks

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,7 @@ Rails.application.routes.draw do
 
   resources :followings
   resources :comments
+  resources :sparks
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
