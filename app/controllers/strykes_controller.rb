827043c95c708587f6e457ed002c3020652afe19
Wesley Merkel <ooesili@gmail.com>
2015-06-04 01:42:56 -0600

    Add infinite scrolling to the feed

--- app/controllers/strykes_controller.rb
+++ app/controllers/strykes_controller.rb
@@ -1,5 +1,17 @@
 class StrykesController < ApplicationController
 
+  def index
+    # create a new comment for the partials
+    @comment = Comment.new
+    # grab columns from the database
+    columns = Stryke.get_columns(current_user, 10, params[:offset].to_i)
+    # render each columns html in place
+    [:new, :top].each do |key|
+      columns[key] = render_strykes key, columns[key]
+    end
+    render json: columns
+  end
+
   def new
     @stryke = Stryke.new
   end
@@ -31,6 +43,16 @@ class StrykesController < ApplicationController
 
   def stryke_params
     params.require(:stryke).permit(:body, :user_id, :image_url)
+  end
+
+  def render_strykes(name, strykes)
+    # render each stryke's html
+    strykes.map do |stryke|
+      render_to_string partial: stryke, locals: {
+        top_class: "#{name}-status"
+      }
+    # combine and return
+    end.join('')
   end
 
 end
