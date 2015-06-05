314c297355906a288003a4c2d850848c2b175ca9
Andrew James <andrewpjames87@gmail.com>
2015-06-04 22:47:30 -0600

    Adjusted video sizing in model

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -10,19 +10,19 @@ class Comment < ActiveRecord::Base
   auto_html_for :body do
     html_escape
     image
-    youtube(:width => 400, :height => 250, :autoplay => false)
-    soundcloud(:width => 400, :height => 250, :autoplay => false)
-    dailymotion(:width => 400, :height => 250, :autoplay => false)
-    google_video(:width => 400, :height => 250, :autoplay => false)
-    vimeo(:width => 400, :height => 250, :autoplay => false)
+    youtube(:width => 275, :height => 175, :autoplay => false)
+    soundcloud(:width => 275, :height => 175, :autoplay => false)
+    dailymotion(:width => 275, :height => 175, :autoplay => false)
+    google_video(:width => 275, :height => 175, :autoplay => false)
+    vimeo(:width => 275, :height => 175, :autoplay => false)
     metacafe
     google_map
     twitter
     flickr
     # instagram currently buggy
-    liveleak(:width => 400, :height => 250, :autoplay => false)
+    liveleak(:width => 275, :height => 175, :autoplay => false)
     ted
-    worldstar(:width => 400, :height => 250, :autoplay => false)
+    worldstar(:width => 275, :height => 175, :autoplay => false)
     hashtag
     link :target => "_blank", :rel => "nofollow"
     simple_format
