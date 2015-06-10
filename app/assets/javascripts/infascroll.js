647e072f809da0aac93c28066247d00f53198ab7
Wesley Merkel <ooesili@gmail.com>
2015-06-09 23:41:23 -0600

    Fix infascroll on public feed

--- app/assets/javascripts/infascroll.js
+++ app/assets/javascripts/infascroll.js
@@ -1,6 +1,6 @@
 $(function() {
   // don't do anything if we are not on the feed page
-  if (window.location.pathname !== '/feed') { return; }
+  if ($('.status-container').size() == 0) { return; }
   // add the loading icon to the footer
   var img = $('<img src="/assets/loading-icon.svg" class="loading-icon" >')
     .appendTo('.foot')
@@ -12,6 +12,7 @@ $(function() {
   var endOfData = false;
   // initial offset
   var offset = $('.new-status').size();
+  var publicFeed = window.location.pathname === '/all';
   // bind scroll event to the document
   jqDoc.on('scroll', function() {
     // don't do anyting if loading or there is no data left
@@ -27,7 +28,10 @@ $(function() {
     // make the request
     $.ajax({
       url: '/strykes',
-      data: {offset: offset},
+      data: {
+        offset: offset,
+        public_feed: publicFeed,
+      },
     }).success(function(data) {
       // update flags
       loading = false;
