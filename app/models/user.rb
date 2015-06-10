234bd7ee2233b09625bb77fcea5425f001e24368
Jake Marwil <rjm02006@gmail.com>
2015-06-09 22:07:35 -0600

    remove validations from setup and fix syntax on validates in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   validates :first_name, :last_name, :username, :interest, :about, :birthday, :gender, presence: true
-  validate :username, uniqueness: true
+  validates :username, uniqueness: true
 
   # Checks that first and last name are only A-Z and a-z (no spaces, numbers, newlines, etc...)
   validates_format_of :first_name, :last_name, :with => /\A[-a-z]+\Z/
