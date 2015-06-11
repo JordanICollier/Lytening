36d9325f71118b854c79570acd9ac0f0118a8637
JordanCollier <jordanicollier@gmail.com>
2015-06-10 21:50:14 -0600

    add comment spacing

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ActiveRecord::Base
 
   # validates :first_name, :last_name, :username, :interest, :about, :birthday, :gender, presence: true
   # validates :username, uniqueness: true
-  # 
+  #
   # # Checks that first and last name are only A-Z and a-z (no spaces, numbers, newlines, etc...)
   # validates_format_of :first_name, :last_name, :with => /\A[-a-z]+\Z/
 
