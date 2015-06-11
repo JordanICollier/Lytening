ed9602855d159d90fac0a46b727ba537decb3f6e
Jake Marwil <rjm02006@gmail.com>
2015-06-10 22:17:40 -0600

    add 500 error page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -28,10 +28,17 @@ class ApplicationController < ActionController::Base
   class OverCapacity < StandardError; end
   rescue_from OverCapacity, with: :over_capacity
 
+  class InternalServer < StandardError; end
+  rescue_from InternalServer, with: :internal_server
+
   private
 
    def over_capacity
      render file: "/public/404.html", status: 404
+   end
+
+   def internal_server
+     render file: "/public/500.html", status: 500
    end
 
 end
